161ddd2598571386df8d86f9fb0547c9d9177b49
erebollo@capgemini.com <erebollo@capgemini.com>
2023-02-25 22:47:28 +0100

    sort sims after final iter

--- js/histogram.js
+++ js/histogram.js
@@ -39,6 +39,7 @@ window.addEventListener("load", (e) => {
 function message(e) {
   let json = JSON.parse(e.data);
   let iter = parseInt(json.iter);
+  console.log("iter => " + iter);
   if (iter > 0) {
     let value = parseFloat(json.value);
     iter_text.text("iter " + iter).attr("x", width-margin.right).attr("y", 2*margin.top);
